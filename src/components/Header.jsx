4f4a63af5986266e7fcd2059a534379cf3239f73
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-16 00:31:24 +0530

    Loaded data by useEffect and declare a useState to hold those data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-
+import { useState } from "react";
+import Card from "./Card";
+import { useEffect } from "react";
 const Header = () => {
+
+    //Declare a state
+    const [card, setCard] = useState([]);
+    console.log(card);
+    //Fetch data
+    useEffect(()=>{
+        fetch('../../public/course_data.json')
+        .then(res => res.json())
+        .then(data => setCard(data));
+    },[]);
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1 className="5xl max-auto p-5">Course Registration</h1>
+            <Card></Card>
         </div>
     );
 };
